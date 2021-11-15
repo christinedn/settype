0927c539a6b36ebbe634a41b9c1cc679da392674
christine <christinedn@users.noreply.github.com>
2021-11-15 00:26:15 -0800

    Added pre/post

--- SetType.h
+++ SetType.h
@@ -22,32 +22,127 @@ class SetType
 {
 public:
     // Constructors
+    /*
+     * Function: Creates a SetType object
+     * Precondition: None
+     * Postcondition: A SetType object will be created
+     */
     SetType();
+    /*
+     * Function: Creates a SetType object with a certain number of buckets
+     * Precondition: None
+     * Postcondition: A SetType object will be created
+     */
     explicit SetType(int numBucks);
+    /*
+     * Function: A copy constructor that copies all the data from other set to this set
+     * Precondition: None
+     * Postcondition: A SetType object will be created with the same data as the set in the parameters
+     */
     SetType(SetType& other);
-
+    /*
+     * Function: A destructor that destroys everything in the set
+     * Precondition: None
+     * Postcondition: Everything in the set will be deleted
+     */
     ~SetType();
-
+    /*
+     * Function: Adds an element into the set
+     * Precondition: None
+     * Postcondition: An element will be added to the set
+     */
     void Add(T elem);
+    /*
+     * Function: Removes an element from the set
+     * Precondition: None
+     * Postcondition: The element will be removed from the set
+     */
     void Remove(T elem);
+    /*
+     * Function: Checks if an element exists in the set
+     * Precondition: None
+     * Postcondition: A boolean value will be returned depending on whether or not the element exists in the set
+     */
     bool Contains(T elem);
+    /*
+     * Function: Clears all the data in the set
+     * Precondition: None
+     * Postcondition: The set will be cleared
+     */
     void MakeEmpty();
+    /*
+     * Function: Returns the number of elements in the set
+     * Precondition: None
+     * Postcondition: The number of elements will be returned
+     */
     int Size() const {
         return numElems;
     }
-
+    /*
+     * Function: Returns the load factor of the set
+     * Precondition: None
+     * Postcondition: The load factor of the set will be returned
+     */
     double LoadFactor() const;
+    /*
+     * Function: Sets the max load of the set
+     * Precondition: None
+     * Postcondition: The max load of the set will be changed to the variable in the parameters
+     */
     void SetMaxLoad(double max);
+    /*
+     * Function: Rehashes the set to a new number of buckets
+     * Precondition: None
+     * Postcondition: The set will be rehashed to a new number of buckets
+     */
     void Rehash(int newNumBuckets);
-
+    /*
+     * Function: Adds an element to the set
+     * Precondition: None
+     * Postcondition: An element will be added to the set
+     */
     SetType operator+(T elem);				      // Add
+    /*
+     * Function: Removes an element from the set
+     * Precondition: None
+     * Postcondition: An element will be remooved from the set
+     */
     SetType operator-(T elem);				      // Remove
+    /*
+     * Function: Finds the union of two sets
+     * Precondition: None
+     * Postcondition: Returns the union of two sets
+     */
     SetType operator+(SetType& otherSet);         // Union
+    /*
+     * Function: Finds the difference of two sets
+     * Precondition: None
+     * Postcondition: Returns the difference of two sets
+     */
     SetType operator-(SetType& otherSet);         // Difference
+    /*
+     * Function: Finds the intersection of two sets
+     * Precondition: None
+     * Postcondition: Returns the intersection of two sets
+     */
     SetType operator*(SetType& otherSet);         // Intersection
+    /*
+     * Function: Copies all the elements from otherSet to this set
+     * Precondition: None
+     * Postcondition: All the elements from otherSet will be copied to this set
+     */
     SetType& operator=(SetType const& otherSet);  // Assignment (does deep copy)
-
+    /*
+     * Function: Resets the iterator
+     * Precondition: None
+     * Postcondition: The iterator will be reset
+     */
     void ResetIterator();	// Reset iterator
+    /*
+     * Function: Finds the next item
+     * Precondition: None
+     * Postcondition: The next item will be returned
+     */
     T GetNextItem();
 
 private:
